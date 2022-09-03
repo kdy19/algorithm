7beb5d091a40c74bf9f4625024fbf8aa32ef605a
kdy19 <qweasd0717@icloud.com>
2022-09-03 20:52:12 +0900

    DP LCS

---
+++ baekjoon/9251.cpp
@@ -0,0 +1,48 @@
+#include <algorithm>
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include <cmath>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+using namespace std;
+
+#define ll long long
+
+const int iInf=(int)1e9;
+const int MOD=(int)1e9+9;
+const int d4x[4]={1,-1,0,0};
+const int d4y[4]={0,0,1,-1};
+const int d8x[8]={-1,-1,0,1,1,1,0,-1};
+const int d8y[8]={0,1,1,1,0,-1,-1,-1};
+
+int dp[1001][1001];
+
+void solve() {
+    string s1, s2;
+    cin >> s1 >> s2;
+
+    for(int i=1; i<=s1.size(); ++i) {
+        for(int j=1; j<=s2.size(); ++j) {
+            if(s1[i-1]==s2[j-1]) 
+                dp[i][j]=dp[i-1][j-1]+1;
+            else
+                dp[i][j]=max(dp[i-1][j], dp[i][j-1]);
+        }
+    }
+
+    cout << dp[s1.size()][s2.size()];
+    
+}
+
+int main(int argc, char* argv[]) {
+	ios_base::sync_with_stdio(false);
+	cout.tie(NULL);
+	cin.tie(NULL);
+
+	solve();
+
+	return 0;
+}
