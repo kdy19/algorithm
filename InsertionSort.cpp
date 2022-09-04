c7fd2f3775c1e1a291418fca73e26d0c8d508d80
kdy19 <qweasd0717@icloud.com>
2022-09-05 02:39:19 +0900

    InsertionSort

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -5,29 +5,19 @@ using namespace std;
 int main() {
 	
 	int n[5]={5,4,3,2,1};
-	int idx=1;
-	
-	cout << idx << ": ";
-	for(int i=0; i<5; ++i)
-		cout << n[i] << ' ';
-	cout << '\n';
-	idx++;
 	
 	for(int i=1; i<5; ++i) {
 		int key=n[i];
 		int j=i-1;
 		while(j>=0&&n[j]>key) {
 			n[j+1]=n[j];
-			j--;
+			--j;
 		}
 		n[j+1]=key;
-		
-		cout << idx << ": ";
-		for(int l=0; l<5; ++l)
-			cout << n[l] << ' ';
-		cout << '\n';
-		idx++;
 	}
+	
+	for(int i=0; i<5; ++i)
+		cout << n[i] << ' ';
 	
 	return 0;
 }
