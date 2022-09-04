a28c6b464dafb14748e0d8cb1a6f8c9dd4b46053
kdy19 <qweasd0717@icloud.com>
2022-09-05 02:39:03 +0900

    BubbleSort

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -5,14 +5,8 @@ using namespace std;
 int main() {
 	
 	int n[5]={5,4,3,2,1};
-	int idx=1, t;
-	
-	cout << "Before: ";
-	for(int i=0; i<5; ++i)
-		cout << n[i] << ' ';
-	cout << "\n\n";
-	
-	cout << "----- Sort start -----\n";
+	int t;
+
 	for(int i=0; i<5; ++i) {
 		for(int j=0; j<5; ++j) {
 			if(n[i]<n[j]) {
@@ -21,19 +15,10 @@ int main() {
 				n[j]=t;
 			}
 		}
-		
-		cout << idx << ": ";
-		for(int l=0; l<5; ++l)
-			cout << n[l] << ' ';
-		cout << '\n';
-		idx++;
 	}
-	cout << "----- Sort end -----\n";
 	
-	cout << "After: ";
 	for(int i=0; i<5; ++i)
 		cout << n[i] << ' ';
-	cout << "\n\n";
-
+	
 	return 0;
 }
